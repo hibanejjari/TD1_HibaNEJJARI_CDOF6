bfc21401ed571a15921a5852f44b368692ed4f66
hibanejjari <159903043+hibanejjari@users.noreply.github.com>
2025-01-13 17:56:28 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+ #!/usr/bin/env python
+ from distutils.core import setup
+ setup(
+ name='{TD1_HibaNEJJARI_CDOF6}',
+ version='1.0',
+ author='{hibanejjari}',
+ license='MIT',
+ long_description=open('README.md').read(),
+ )
